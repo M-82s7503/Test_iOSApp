464393f1c7b7fd0863bc2603b9b1a7b9a0a73a21
N-61336535R-N <N.math.mryo.N@gmail.com>
2017-11-28 23:00:32 +0900

    Initial Commit

---
+++ cats-eye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cats-eye
+//
+//  Created by Matsumoto Ryo on 2017/11/28.
+//  Copyright © 2017年 Univ-ryukyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
